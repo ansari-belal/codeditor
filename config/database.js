f3651f6bdeb1e2318cd1bc4bcf0b35acb4de7e35
ansari-belal <ansaribelal184@gmail.com>
2023-10-23 22:36:16 +0530

    my commit

--- config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('mydatabase', 'root', 'mydatabase', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-module.exports = sequelize;
